38c5e840cfe1fa43d98453c33c4ca3908abed99e
AlekseyVlasov <aleksvvvvv56@gmail.com>
2025-04-23 18:46:42 +0300

    Add DataBase

---
+++ UAWalks/UAWalks.API/Data/UAWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UAWalks.API.Model.Domain;
+
+namespace UAWalks.API.Data
+{
+    public class UAWalksDbContext: DbContext
+    {
+        public UAWalksDbContext(DbContextOptions<UAWalksDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+    }
+}
---
+++ UAWalks/UAWalks.API/Model/Domain/Difficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace UAWalks.API.Model.Domain
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
